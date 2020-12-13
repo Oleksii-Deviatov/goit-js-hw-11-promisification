7eaec2a705fa0d380871ac7d2a4911fbc98fb4fd
Alexeii Deviatov <65559135+lewyk@users.noreply.github.com>
2020-12-13 21:21:52 +0200

    Update task2.js

--- src/task2.js
+++ src/task2.js
@@ -1,7 +1,3 @@
-// Задание 2
-// Перепиши функцию toggleUserState() так, чтобы она не использовала callback - функцию callback,
-//     а принимала всего два параметра allUsers и userName и возвращала промис.
-
 const users = [
   { name: 'Mango', active: true },
   { name: 'Poly', active: false },
@@ -20,14 +16,5 @@ function toggleUserState(allUsers, userName) {
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
